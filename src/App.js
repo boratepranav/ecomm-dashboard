8ef5b29a0ea303536ba9d95270ce1dd838d152b7
Pranav <boratepranav@gmail.com>
2024-11-07 13:10:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>E-Comm Website</h1>
+      <button>Noraml  button</button>
+      <Button>Noraml  button</Button>
+ 
+     
+</div>
+  );
+}
+
+export default App;
